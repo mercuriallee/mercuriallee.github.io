50eb76696df3d6f585175205e0567434503ba5e8
Mercurial Lee <mercurialee@gmail.com>
2023-06-16 13:28:54 +0800

    Publish HuaRongDao debug version.+

--- hrd/main.dart.js
+++ hrd/main.dart.js
@@ -20337,7 +20337,7 @@ $1(a){var s=$.cj
 s=(s==null?$.cj=A.f7(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/2a3401c9bbb5a9a9aec74d4f735d18a9dd3ebf2d/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/45f6e009110df4f34ec2cf99f63cf73b71b7a420/":s)+a},
 $S:108}
 A.a34.prototype={
 $1(a){this.a.remove()
