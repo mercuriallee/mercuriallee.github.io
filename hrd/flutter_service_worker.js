21e9d73c4098ebdda70152c4e6688d12d6ac3a8e
Mercurial Lee <mercurialee@gmail.com>
2023-07-26 13:27:41 +0800

    2023年 7月26日 星期三 13时27分41秒 CST

--- hrd/flutter_service_worker.js
+++ hrd/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "a94b806879901b8a39e9c2132430e375",
-"index.html": "5019274324dc2d564848d9a40c5925a0",
-"/": "5019274324dc2d564848d9a40c5925a0",
-"main.dart.js": "1a20673514ca05d9fe57dde19f2fb28d",
+"index.html": "38a26ef9fb2424e71356794895dc3ce5",
+"/": "38a26ef9fb2424e71356794895dc3ce5",
+"main.dart.js": "67f02f897cf2fd4ea59a8122d1cb2f8d",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
