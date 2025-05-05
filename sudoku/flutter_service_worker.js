ff31b52301a3462c761253096adbd7b5841f2dc8
Mercurial Lee <mercurialee@gmail.com>
2025-05-05 16:29:30 +0800

    sudoku: refine layout of homepage.

--- sudoku/flutter_service_worker.js
+++ sudoku/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "dda5ecf506f236cbbfcec4d6f7627909",
+const RESOURCES = {"flutter_bootstrap.js": "c573f009644cd137ed84166b4d96a806",
 "version.json": "185f26ebeddf704501049ca172567d3d",
 "index.html": "2b44ddb98a5708b4e2cb29e943dd1cd3",
 "/": "2b44ddb98a5708b4e2cb29e943dd1cd3",
-"main.dart.js": "6332a2d5a55844e95ec5e007ba8480d8",
+"main.dart.js": "aaaf8b78972c55c10d5f493bd118cd84",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
