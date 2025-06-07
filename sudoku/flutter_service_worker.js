f360a909140056557e70857aff00599391051222
Mercurial Lee <mercurialee@gmail.com>
2025-06-07 13:37:16 +0800

    sudoku: optimization of generateBinarysArray of game_logic.dart.

--- sudoku/flutter_service_worker.js
+++ sudoku/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "89ab1fef357b868bc6464e9db28b3054",
+const RESOURCES = {"flutter_bootstrap.js": "c08c1e7bd725f115ac37012d007070ec",
 "version.json": "d376e9041101b7601f43492c4a96cdc3",
 "index.html": "2b44ddb98a5708b4e2cb29e943dd1cd3",
 "/": "2b44ddb98a5708b4e2cb29e943dd1cd3",
-"main.dart.js": "5e5c3f1daf604c8941dec439fe7e8e0d",
+"main.dart.js": "9251b0cb86c78a3e080ee1e76ca1d4c8",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
